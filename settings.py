a4a00803ab4976f366bf7e0c41b30c0c3241858e
KP <88257784+SpicyBeeswaxunicornsauce@users.noreply.github.com>
2021-08-24 20:54:01 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,18 +9,26 @@ class Settings:
         self.bg_image = pygame.image.load('starfield.png').convert()
         self.bg_img_rect = self.bg_image.get_rect()
         self.caption = 'Alien Invasion'
-        self.right_ship_speed = 1
-        # self.left_ship_speed = self.right_ship_speed + ((self.right_ship_speed - 1) - .4)
-        self.left_ship_speed = 1
         # Bullet Settings
-        self.bullet_speed = 1
         self.bullet_width, self.bullet_height = 5, 20
         self.bullet_color = (182, 182, 182)
         self.bullets_allowed = 3
         # Alien settings
-        self.alien_speed = 0.6
         self.fleet_drop_speed = 20
+        self.ship_limit = 2
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change during the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 1
+        self.alien_speed = 0.6
+
         self.fleet_direction = 1
         # self.fleet_direction is which way the aliens are going.
         # 1 means right; -1 means left.
-        self.ship_limit = 2
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
